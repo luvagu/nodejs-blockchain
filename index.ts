9fb44fc651a9a914ca71b63a0551bb2d50013314
luvagu <luiavag@gmail.com>
2021-05-27 17:14:00 -0300

    created transaction class

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import * as crypto from 'crypto'
 
 class Transaction {
+  constructor(
+    public amount: number,
+    public payer: string, // public key
+    public payee: string, // public key
+  ) {}
 
+  toString() {
+    return JSON.stringify(this)
+  }
 }
 
-class Block {}
-
-class Chain {}
-
-class Wallet {}
